d076ae06a92705bf89a4e23c6fae15631e23758b
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-10-10 14:39:25 -0400

    added peachtree image header

---
+++ client/src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../UIELements/assets/logo.jpg';
+
+import './Header.css';
+const Header = (props) => {
+  return <div className="header__container">
+      <img src={logo} alt="logo"/>
+  </div>;
+};
+
+export default Header;
