e3f973b59a9657b85cfcf517f067ec161494548d
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-10-10 14:03:08 -0400

    skeleton and assets have been implemented

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV = "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+});
+
+app.listen((err) => console.log(`PORT LISTENING ON ${PORT}`));
